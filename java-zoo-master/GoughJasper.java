d6ad9e92f4f0b7d3565ff60df10d685037ba54d9
Jasper Gough <jasper.gough@mrjaffesclass.com>
2024-01-03 11:42:37 -0800

    bad to the bone >:)

---
+++ java-zoo-master/GoughJasper.java
@@ -0,0 +1,18 @@
+public class GoughJasper extends Animal
+{
+    public GoughJasper() {
+        super("Jasper Gough", "white", 2);
+    }
+    public String getName() {
+        return "Albert Spinestein";
+    }
+    public String getDisposition() {
+        return "bad to the bone >:)";
+    }
+    public String speak() {
+        return "I've got a BONE to pick with you";
+    }
+    public String getType() {
+        return "skeleton";
+    }
+}
